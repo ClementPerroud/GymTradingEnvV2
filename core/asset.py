7b1fdeb572a55a8e341eb3002465bf928f41bf62
Clement Perroud <c.perroud@commandstrategy.com>
2025-01-24 16:04:51 +0100

    Correct asset

--- core/asset.py
+++ core/asset.py
@@ -1,12 +1,14 @@
-from dataclasses import dataclass
+class Asset(object):
 
-@dataclass
-class Asset:
-    name : str
+    def __init__(self, name : str):
+        self.name = name
+
     def __eq__(self, other) -> bool:
         if not isinstance(other, Asset): return False
         return self.name == other.name
+
     def __repr__(self) -> str:
         return self.name
+
     def __hash__(self) -> int:
         return self.__repr__().__hash__()
