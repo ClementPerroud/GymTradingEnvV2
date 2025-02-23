5c5d0dba46de76eb0a7fa52cc03fd5100559cc1a
Clement Perroud <c.perroud@commandstrategy.com>
2025-02-23 10:40:09 +0100

    Adding feature the infos_manager

--- infos_manager.py
+++ infos_manager.py
@@ -6,6 +6,9 @@ from typing import List
 from collections import deque
 import numpy as np
 
+from .core.pair import Pair
+from .core.asset import Asset
+from .core.portfolio import Portfolio, PortfolioExposition
 from .renderers.renderer import AbstractRenderer
 from .managers.portfolio import PortfolioManager
 from .exchanges.responses import TickerResponse
@@ -13,10 +16,11 @@ from .utils.speed_analyser import SpeedAnalyser
 
 
 class InfosManager(AbstractRenderer):
-    def __init__(self, pairs, quote_asset) -> None:
+    def __init__(self, pairs : list[Pair], quote_asset) -> None:
         super().__init__()
         self.quote_asset = quote_asset
         self.pairs = pairs
+        self.assets = set([pair.asset for pair in pairs] + [pair.quote_asset for pair in pairs])
         self.infos_func = []
 
     async def reset(self, seed = None):
@@ -29,6 +33,7 @@ class InfosManager(AbstractRenderer):
         self.infos_func.append(func)
     
     async def _get_infos(self):
+        # Retrieve data
         date = await self.time_manager.get_current_datetime()
         portfolio = await self.exchange_manager.get_portfolio()
         results = await self.gather(
@@ -36,15 +41,27 @@ class InfosManager(AbstractRenderer):
             self.portfolio_manager.exposition(portfolio= portfolio, date= date, quote_asset= self.quote_asset),
             *[self.exchange_manager.get_ticker(pair= pair, date=date) for pair in self.pairs],
         )
-        portfolio_valuation, portfolio_exposition = results[0], results[1]
+        portfolio_valuation = results[0]
+        portfolio_exposition : PortfolioExposition =  results[1]
         ticker_dict : List[TickerResponse] = dict(zip(self.pairs, results[2:]))
+
+        # Process data
+        portfolio_per_asset  = {asset : portfolio.get_position(asset = asset) for asset in self.assets}
+        portfolio_exposition_per_asset = {asset : portfolio_exposition.get_position(asset = asset) for asset in self.assets}
+        for _dict_per_asset in [portfolio_per_asset, portfolio_exposition_per_asset]:
+            for asset in self.assets: 
+                if _dict_per_asset[asset] is None: _dict_per_asset[asset] = 0
+                else: _dict_per_asset[asset] = _dict_per_asset[asset].amount
+
         infos = {
             "date": date,
             "portfolio_valuation" : float(portfolio_valuation.amount),
             "portfolio_valuation_asset" : portfolio_valuation.asset.name,
-            "portfolio" : portfolio,
-            "position_exposition" : portfolio_exposition,
-            **{f"price_{pair}" : float(ticker_dict[pair].close.amount) for pair in self.pairs}
+            **{f"portfolio_{asset}" : float(portfolio_per_asset[asset]) for asset in self.assets},
+            **{f"position_exposition_{asset}" : float(portfolio_exposition_per_asset[asset]) for asset in self.assets},
+            **{f"price_{pair}" : float(ticker_dict[pair].close.amount) for pair in self.pairs},
+            "quote_asset" : self.quote_asset.name,
+            "assets" : [asset.name for asset in [asset for asset in self.assets if asset != self.quote_asset]],
         }
 
         return date, infos
